8f90b7d6c396c658067afcc89c3f394044fc433f
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-02-10 23:15:07 +0700

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/654ddaf472f18b799600b.jpg", caption=f"» ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ [ᴅᴇᴠɪʟs ʜᴇᴀᴠᴇɴ]({link}) ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ [ᴅᴇᴠɪʟs ʜᴇᴀᴠᴇɴ]({link}) ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                    photo="https://telegra.ph/file/58e9d02b8e44a4f5f030c.jpg", caption=f"Halo {msg.from_user.mention} anda harus bergabung [amangsupport]({link}) terlebih dahulu untuk menggunakan saya. Setelah bergabung silahkan /start lagi!",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> ᴅᴇᴠɪʟs ʜᴇᴀᴠᴇɴ", url=link),
+                                InlineKeyboardButton("</> Join Grup", url=link),
                             ]
                         ]
                     )
